4d35e1b3884ba287d78ac5adba6f6099e01d4259
Yassine Doghri <yassine@doghri.fr>
2022-08-20 17:00:27 +0000

    feat: move astro-i18next config in a standalone file to load it for CLI commands
    update config properties for consistency
    
    BREAKING CHANGE: config is now a standalone file + some property names
    have changed for better clarity and consistency
    
    - `baseLocale` is now `defaultLanguage`
    - `supportedLocales` is now `supportedLanguages``

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,14 +3,8 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { generate } from "./generate";
-import { isLocale } from "./utils";
-
-interface GenerateArgs {
-  path: string;
-  defaultLanguage: string;
-  languages: string[];
-  output: string;
-}
+import { loadConfig, normalizePath } from "./middlewares";
+import { GenerateArgs } from "./types";
 
 yargs(hideBin(process.argv))
   .usage("usage: $0 <command>")
@@ -21,61 +15,39 @@ yargs(hideBin(process.argv))
       return yargs
         .positional("path", {
           type: "string",
-          description: "Path to base pages folder",
-          default: "./src/pages",
+          description: "Path to the Astro project folder",
+          default: "./",
         })
-        .options({
-          "default-language": {
-            alias: "d",
-            type: "string",
-            description: "Set the default language",
-            demandOption: true,
-          },
-          languages: {
-            alias: "l",
-            type: "array",
-            string: true,
-            description: "Provide a list of locales to generate",
-            demandOption: true,
-          },
-          output: {
-            alias: "o",
-            type: "string",
-            description:
-              "Set the output of the generated pages if different from input",
-          },
-        })
-        .check((argv) => {
-          const { "default-language": defaultLanguage, languages } = argv;
-
-          const errors = [];
-
-          if (!isLocale(defaultLanguage)) {
-            errors.push(`"${defaultLanguage}" is not a valid locale!`);
-          }
-
-          languages.forEach((language) => {
-            if (!isLocale(language)) {
-              errors.push(`"${language}" is not a valid locale!`);
-            }
-          });
-
-          if (errors.length > 0) {
-            throw new Error(errors.join("\n"));
-          }
-
-          return true;
+        .option("output", {
+          alias: "o",
+          type: "string",
+          description:
+            "Set the output of the generated pages if different from input",
         });
     },
-    (argv) => {
+    async (argv) => {
       if (argv.verbose) {
         console.info(`Generating localized pages: ${argv.languages}`);
       }
 
-      generate(argv.path, argv.defaultLanguage, argv.languages, argv.output);
+      const pagesPath = argv.path + "src/pages";
+
+      generate(
+        pagesPath,
+        argv.config.defaultLanguage,
+        argv.config.supportedLanguages,
+        argv.output
+      );
     }
   )
+  .middleware([normalizePath, loadConfig], true)
   .options({
+    config: {
+      alias: "c",
+      type: "string",
+      description:
+        "Set the output of the generated pages if different from input",
+    },
     verbose: {
       alias: "v",
       type: "boolean",
