6125aaa26caed47224ab20400df5ee5210eb746f
Yassine Doghri <yassine@doghri.fr>
2022-08-21 14:59:40 +0000

    docs: update README.md to include plugins + CLI commands

--- example/astro.config.js
+++ example/astro.config.js
@@ -6,9 +6,6 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-i18next.yassinedoghri.com/",
-  experimental: {
-    integrations: true,
-  },
   integrations: [
     sitemap({
       i18n: {
--- example/tailwind.config.cjs
+++ example/tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
