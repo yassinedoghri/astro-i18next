7ba845e3de82413051be501c51f7a98db4c1fb3e
Yassine Doghri <yassine@doghri.fr>
2022-08-21 10:52:16 +0000

    build: export astro-i18next config and options types for typehints

--- example/astro-i18next.config.ts
+++ example/astro-i18next.config.ts
@@ -1,12 +1,15 @@
-export default {
+import { AstroI18nextConfig } from "astro-i18next";
+
+const config: AstroI18nextConfig = {
   defaultLanguage: "en",
   supportedLanguages: ["en", "fr"],
   i18next: {
     debug: true,
-    initImmediate: false,
     backend: {
       loadPath: "./src/locales/{{lng}}.json",
     },
   },
   i18nextPlugins: { fsBackend: "i18next-fs-backend" },
 };
+
+export default config;
