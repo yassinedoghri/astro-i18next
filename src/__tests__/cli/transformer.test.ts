4d74e0b3d1d03c40ca9090b82fb4d171cd4b84a0
Yassine Doghri <yassine@doghri.fr>
2022-08-27 15:10:44 +0000

    fix(generate): inject changeLanguage statement after imports and before frontmatter logic
    fixes #23

--- src/__tests__/cli/transformer.test.ts
+++ src/__tests__/cli/transformer.test.ts
@@ -13,61 +13,61 @@ describe("transformer(...)", () => {
       name: "with no i18next import",
       actual: ``,
       expected:
-        'import { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with i18next import",
       actual: `import i18next from "i18next";`,
       expected:
-        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with i18next import + named imports",
       actual: `import {t} from "i18next";`,
       expected:
-        'import { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with i18next import + named imports + language switch",
       actual: `import { t, changeLanguage } from "i18next";changeLanguage("en");\n`,
       expected:
-        'import { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with i18next import + global import + named imports + language switch",
       actual: `import i18next, { t, changeLanguage } from "i18next";changeLanguage("en");`,
       expected:
-        'import i18next, { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import i18next, { t, changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with changeLanguage being called from i18next import",
       actual: `import i18next from "i18next";i18next.changeLanguage("en");`,
       expected:
-        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with changeLanguage being called from i18next import + named imports",
       actual: `import i18next, { changeLanguage } from "i18next";i18next.changeLanguage("en");`,
       expected:
-        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import i18next, { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with other imports",
       actual: `import foo from "foo";\nimport bar from "bar";\nimport {baz} from "baz";\nimport { changeLanguage } from "i18next";changeLanguage("en");`,
       expected:
-        'import foo from "foo";\nimport bar from "bar";\nimport { baz } from "baz";\nimport { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n',
+        'import foo from "foo";\nimport bar from "bar";\nimport { baz } from "baz";\nimport { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\n',
     },
     {
       name: "with logic after imports",
       actual: `import { changeLanguage } from "i18next";\n\nconst a = 1 + 2;\nchangeLanguage("en");`,
       expected:
-        'import { changeLanguage } from "i18next";\nconst a = 1 + 2;\n\nchangeLanguage("fr");\n',
+        'import { changeLanguage } from "i18next";\n\nchangeLanguage("fr");\n\nconst a = 1 + 2;\n',
     },
     {
       name: "with relative imports",
       actual: `import Foo from "../Foo.astro";\nimport Bar from "../../Bar.astro";\nimport { baz } from "./baz";`,
       expected:
-        'import { changeLanguage } from "i18next";\nimport Foo from "../../Foo.astro";\nimport Bar from "../../../Bar.astro";\nimport { baz } from "../baz";\n\nchangeLanguage("fr");\n',
+        'import { changeLanguage } from "i18next";\nimport Foo from "../../Foo.astro";\nimport Bar from "../../../Bar.astro";\nimport { baz } from "../baz";\n\nchangeLanguage("fr");\n\n',
     },
   ];
 
