bb7ab0f318feeb7bd4243ea805d09f55634b1ecf
Yassine Doghri <yassine@doghri.fr>
2022-08-27 13:46:49 +0000

    fix(build): remove components and utils from build + set components export to src
    update packages to latest
    
    fixes #18

--- build.cjs
+++ build.cjs
@@ -3,12 +3,7 @@
 require("esbuild")
   .build({
     bundle: true,
-    entryPoints: [
-      "src/index.ts",
-      "src/utils.ts",
-      "src/components/index.mts",
-      "src/cli/index.ts",
-    ],
+    entryPoints: ["src/index.ts", "src/cli/index.ts"],
     outdir: "dist",
     external: [
       "i18next",
@@ -28,9 +23,6 @@ require("esbuild")
     sourcemap: "inline",
     sourcesContent: false,
     allowOverwrite: true,
-    loader: {
-      ".astro": "file",
-    },
     assetNames: "[dir]/[name]",
   })
   .catch(() => process.exit(1));
