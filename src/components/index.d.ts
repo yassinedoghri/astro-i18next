3818054ba1f4c80f00dbbeccb46a2f9cb2795ffe
Yassine Doghri <yassine@doghri.fr>
2022-08-27 13:46:49 +0000

    fix(build): remove components and utils from build + set components export to src
    update packages to latest
    
    fixes #18

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,1 +1,3 @@
-declare module "*.astro";
+export { default as Trans } from "./Trans.astro";
+export { default as LanguageSelector } from "./LanguageSelector.astro";
+export { default as HeadHrefLangs } from "./HeadHrefLangs.astro";
