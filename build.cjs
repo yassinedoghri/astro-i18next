17afecc7f123d8fc40c41852fc5996ecad05ce85
Josh Kramer <josh@kramer.run>
2022-10-25 17:10:23 -0400

    refactor: expose /utils to library consumers

--- build.cjs
+++ build.cjs
@@ -3,7 +3,7 @@
 require("esbuild")
   .build({
     bundle: true,
-    entryPoints: ["src/index.ts", "src/cli/index.ts"],
+    entryPoints: ["src/index.ts", "src/utils.ts", "src/cli/index.ts"],
     outdir: "dist",
     external: [
       "i18next",
